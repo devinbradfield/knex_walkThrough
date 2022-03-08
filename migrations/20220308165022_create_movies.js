d1339388e67ffea775dfa1939bf789cf13d12d3b
devinbradfield <devinbradfield@gmail.com>
2022-03-08 10:25:18 -0700

    finised task

---
+++ migrations/20220308165022_create_movies.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('movies', table => {
+      table.increments('id');
+      table.string('title')
+      table.string('genre')
+      table.date('release_date')
+      table.timestamps(true, true)
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('movies')
+};
